d879c88e09012bc9ae20ceff274d09e307170807
koko666 <koko_666@mail.ru>
2016-01-02 13:53:05 +0200

    Win!!

---
+++ DateTimeCreating/DateTimeExample.cs
@@ -0,0 +1,16 @@
+using System;
+
+class DateTimeExample
+{
+    static void Main()
+    {
+        DateTime halloween = new DateTime(2016, 10, 31);
+        Console.WriteLine(halloween);
+
+        DateTime julyMorning = new DateTime(2016, 7, 1, 5, 52, 0);
+        Console.WriteLine(julyMorning);
+
+		DateTime dayAfterHalloween = halloween.AddDays(1);
+		Console.WriteLine(dayAfterHalloween);
+    }
+}
