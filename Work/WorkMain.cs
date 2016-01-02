6fe7331b8b024beb8b3dc243bc636f99302daf2e
koko666 <koko_666@mail.ru>
2016-01-02 22:47:57 +0200

    12

--- Work/WorkMain.cs
+++ Work/WorkMain.cs
@@ -1,80 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
-using Work;
 
-public enum MakeCars
+namespace Work
 {
-    MERCEDES,
-    BMW,
-    AUDI,
-    SUBARU,
-    PORSCHE,
-    TOYOTA,
-    HONDA,
-    BENTLEY
-}
-
-class WorkMain
-{
-    static void Main()
+    public enum MakeCars
     {
-        MakeCars bmwCars = MakeCars.BMW;
-        MakeCars audiCars = MakeCars.AUDI;
-        MakeCars bentleyCars = MakeCars.BENTLEY;
-        MakeCars toyotaCars=MakeCars.TOYOTA;
+        MERCEDES,
+        BMW,
+        AUDI,
+        SUBARU,
+        PORSCHE,
+        TOYOTA,
+        HONDA,
+        BENTLEY
+    }
 
-        Console.WriteLine(bentleyCars);
-        Console.WriteLine(bmwCars);
-        Console.WriteLine(audiCars);
-
-        Random rnd=new Random();
-        int[] nums=new int[rnd.Next(1,100)];
-
-        for (int i = 0; i < nums.Length; i++)
+    class WorkMain
+    {
+        static void Main()
         {
-            nums[i] = rnd.Next();
-        }
+            MakeCars bmwCars = MakeCars.BMW;
+            MakeCars audiCars = MakeCars.AUDI;
+            MakeCars bentleyCars = MakeCars.BENTLEY;
+            MakeCars toyotaCars = MakeCars.TOYOTA;
 
-        Console.Write(string.Join(",",nums));
-        Console.WriteLine();
-        Console.WriteLine();
+            Console.WriteLine(bentleyCars);
+            Console.WriteLine(bmwCars);
+            Console.WriteLine(audiCars);
 
-        List<int> numList=new List<int>(nums);
-        numList.Sort();
-        Console.WriteLine(string.Join(";",numList));
+            Random rnd = new Random();
+            int[] nums = new int[rnd.Next(1, 100)];
 
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = rnd.Next();
+            }
 
-        string txt = "       luck  ";
-        DateTime now=DateTime.Now;
-        Console.WriteLine(txt.Trim().ToUpper());
+            Console.Write(string.Join(",", nums));
+            Console.WriteLine();
+            Console.WriteLine();
 
-        now.AddYears(5);
-        Car myCar=new Car();
-        myCar.make = "Mercedes";
-        myCar.model = "G 63 AMG";
-        myCar.power = 544;
-        Console.WriteLine(myCar.make+" " + myCar.model + " "+ myCar.power+"Hp");
-
-        
+            List<int> numList = new List<int>(nums);
+            numList.Sort();
+            Console.WriteLine(string.Join(";", numList));
 
 
+            string txt = "       luck  ";
+            DateTime now = DateTime.Now;
+            Console.WriteLine(txt.Trim().ToUpper());
 
-        Car lastCar=new Car();
-        lastCar.make = "BMW";
-        lastCar.model = "X6 M";
-        lastCar.power = 567;
-        Console.WriteLine(lastCar.make+" "+ lastCar.model+" "+lastCar.power+"Hp");
+            now.AddYears(5);
+            Car myCar = new Car();
+            myCar.make = "Mercedes";
+            myCar.model = "G 63 AMG";
+            myCar.power = 544;
+            Console.WriteLine(myCar.make + " " + myCar.model + " " + myCar.power + "Hp");
 
-        int year = 2016;// int.Parse(Console.ReadLine());
-        bool Leap = DateTime.IsLeapYear(year);
+            Car lastCar = new Car();
+            lastCar.make = "BMW";
+            lastCar.model = "X6 M";
+            lastCar.power = 567;
+            Console.WriteLine(lastCar.make + " " + lastCar.model + " " + lastCar.power + "Hp");
 
-        if (Leap)
-        {
-            Console.WriteLine("The year {0} is leap", year );
-        }
-        else
-        {
-            Console.WriteLine("The year {0} is not leap", year);
+            int year = 2016; // int.Parse(Console.ReadLine());
+            bool Leap = DateTime.IsLeapYear(year);
+
+            if (Leap)
+            {
+                Console.WriteLine("The year {0} is leap", year);
+            }
+            else
+            {
+                Console.WriteLine("The year {0} is not leap", year);
+            }
         }
     }
 }
