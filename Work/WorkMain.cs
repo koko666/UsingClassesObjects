1bd60d0aedf368fd646625f53c885c6165b28d93
koko666 <koko_666@mail.ru>
2016-01-02 17:33:16 +0200

    Class

--- Work/WorkMain.cs
+++ Work/WorkMain.cs
@@ -3,18 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Work;
 
-namespace Work
+
+class WorkMain
 {
-    class WorkMain
+    static void Main()
     {
-        static void Main()
-        {
-            DateTime now=DateTime.Now;
-            DateTime nnnDateTime= now.AddHours(12);
-            
-            Console.WriteLine(now.AddHours(12));
-            Console.WriteLine(nnnDateTime.Hour  );
-        }
+        Student studNew = new Student();
+        studNew.FirstName = "Pesho";
+        studNew.LastName = "Ivanov";
+
+        //Car myCar=new Car();
+
+        Console.WriteLine(studNew.FirstName+" "+ studNew.LastName  );
+
+        Car myCar=new Car();
+        myCar.marka = "Mersedes";
+        myCar.model = "AMG 63";
+        myCar.power = 650;
+
+
+        Console.WriteLine(myCar.marka+" " + myCar.model + " "+ myCar.power+"h");
+
+
+
     }
 }
+
+class Student
+{
+    public string FirstName;
+    public string LastName;
+}
+
